18d8b8f2eeb8ecf079e54435ca37d7c420d7741c
rbardet <robin.bardet.tomczak@gmail.com>
2025-11-14 07:55:09 +0100

    Fix: type definition error

--- srcs/ft_shield.h
+++ srcs/ft_shield.h
@@ -14,13 +14,18 @@
 #define LOCK_FILE "/var/lock/ft_shield.lock"
 #define LOGIN "rbardet-"
 #define PORT 4242
-#define MAX_CLIENT 3
+#define MAX_USER 3
 #define MAX_EVENT 10
+#define LIMIT_REACH "Too many user connected, retry later"
+#define FAILED_CONNECTION "Failed to connect"
 
 typedef struct {
 	int fd;
-	struct epoll_event *event;
+	struct epoll_event event;
+	struct epoll_event events[MAX_EVENT];
 } EPOLL_STRUCT;
+
+extern int userNb;
 
 #define EPOLL_ERROR ((EPOLL_STRUCT){.fd = -1})
 
@@ -29,5 +34,10 @@ void create_lock();
 void delete_lock();
 
 void run_server();
+
+void accept_user(int sockfd, EPOLL_STRUCT epoll);
+void refuse_user(int sockfd);
+void disconnect_user(int userfd);
+void read_input(int userfd);
 
 #endif
--- srcs/socket.c
+++ srcs/socket.c
@@ -1,5 +1,7 @@
 #include "ft_shield.h"
 
+int userNb = 0;
+
 static int init_socket() {
 	int sockfd = socket(AF_INET, SOCK_STREAM, 0);
 	if (sockfd < 0) {
@@ -15,25 +17,25 @@ static int init_socket() {
 		return (-1);
 	}
 
-	if (listen(sockfd, MAX_CLIENT) < 0) {
+	if (listen(sockfd, MAX_USER) < 0) {
 		return (-1);
 	}
 	
 	return (sockfd);
 }
 
-static EPOLL_STRUCT *init_epoll(int sockfd) {
-	EPOLL_STRUCT *epoll;
+static EPOLL_STRUCT init_epoll(int sockfd) {
+	EPOLL_STRUCT epoll;
 
-	epoll->fd = epoll_create1(0);
-	if (epoll->fd < 0) {
-		return (NULL);
+	epoll.fd = epoll_create1(0);
+	if (epoll.fd < 0) {
+		return (EPOLL_ERROR);
 	}
 
-	epoll->event->events = EPOLLIN;
-	epoll->event->data.fd = sockfd;
-	if (epoll_ctl(epoll->fd, EPOLL_CTL_ADD, sockfd, &epoll->event) < 0) {
-		return (NULL);
+	epoll.event.events = EPOLLIN;
+	epoll.event.data.fd = sockfd;
+	if (epoll_ctl(epoll.fd, EPOLL_CTL_ADD, sockfd, &epoll.event) < 0) {
+		return (EPOLL_ERROR);
 	}
 
 	return (epoll);
@@ -45,9 +47,27 @@ void run_server() {
 		return ;
 	}
 
-	EPOLL_STRUCT *epoll = init_epoll(sockfd);
-	if (epoll->fd < 0) {
+	EPOLL_STRUCT epoll = init_epoll(sockfd);
+	if (epoll.fd < 0) {
 		return;
 	}
-	
+
+	// while (1) {
+	// 	int eventNb = epoll_wait(epoll.fd, epoll.events, MAX_EVENT, -1);
+	// 	if (eventNb == -1) {
+	// 		continue ;
+	// 	}
+
+	// 	for (int i = 0; i < eventNb; i++) {
+	// 		if (epoll.events[i].data.fd == sockfd && userNb < MAX_USER) {
+	// 			accept_user(sockfd, epoll);
+	// 		} else {
+	// 			refuse_user(sockfd);
+	// 		}
+
+	// 		if (epoll.events->data.fd != sockfd) {
+	// 			read_input(epoll.events[i].data.fd);
+	// 		}
+	// 	}
+	// }
 }
