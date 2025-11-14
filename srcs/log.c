d46d4bcb34ace777499effa8e1926725468bade2
rbardet <robin.bardet.tomczak@gmail.com>
2025-11-14 12:33:29 +0100

    Feat: add utils func to remove \n

--- srcs/log.c
+++ srcs/log.c
@@ -6,12 +6,16 @@ void create_log() {
 	logFile = fopen(LOG_FILE, "w+");
 }
 
+void remove_nl(const char *buff) {
+	char *i = strchr(buff, '\n');
+	if (i) {
+		i = 0;
+	}
+}
+
 void log_event(char *log, LOG_TYPE __TYPE__) {
 	if (__TYPE__ == LOG_INPUT) {
-		char *i = strchr(log, '\n');
-		if (i) {
-			i = 0;
-		}
+		remove_nl(log);
 		write(logFile->_fileno, LOG_USER_INPUT, strlen(LOG_USER_INPUT));
 		write(logFile->_fileno, log, strlen(log));
 	} else {
