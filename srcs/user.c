8138a65e59d270cd837b8ad6f1783268c1bcc520
rbardet <robin.bardet.tomczak@gmail.com>
2025-11-14 08:49:14 +0100

    Add: user action logged

--- srcs/user.c
+++ srcs/user.c
@@ -7,12 +7,14 @@ void accept_user(int sockfd, EPOLL_STRUCT epoll) {
 	}
 
 	if (epoll_ctl(epoll.fd, EPOLL_CTL_ADD, userfd, &epoll.event) < 0) {
+		log_event(LOG_EPOLL_FAILED);
 		write(userfd, FAILED_CONNECTION, sizeof(FAILED_CONNECTION));
 		close(userfd);
 		return ;
 	}
 
 	userNb++;
+	log_event(LOG_NEW_USER);
 }
 
 void refuse_user(int sockfd) {
@@ -21,6 +23,7 @@ void refuse_user(int sockfd) {
 		return ;
 	}
 
+	log_event(LOG_LIMIT_REACH);
 	write(userfd, LIMIT_REACH, sizeof(LIMIT_REACH));
 	close(userfd);
 }
@@ -29,8 +32,18 @@ void disconnect_user(int userfd, EPOLL_STRUCT epoll) {
 	close(userfd);
 	epoll_ctl(epoll.fd, EPOLL_CTL_DEL, userfd, &epoll.event);
 	userNb--;
+	log_event(LOG_DISCONNECT_USER);
 }
 
-void read_input(int userfd) {
-	(void)userfd;
+void read_input(int userfd, EPOLL_STRUCT epoll) {
+	char buffer[BUFFER_SIZE];
+	int len = read(userfd, buffer, sizeof(buffer));
+	if (len == 0) {
+		disconnect_user(userfd, epoll);
+		return ;
+	} else if (len < 0) {
+		return ;
+	}
+
+	log_event(buffer);
 }
