723881e848a2b1d812b47af3317518ad63272c22
rbardet <robin.bardet.tomczak@gmail.com>
2025-11-24 07:29:38 +0100

    Fix: systemd file path

--- srcs/systemd.c
+++ srcs/systemd.c
@@ -1,10 +1,11 @@
 #include "ft_shield.h"
 
 void systemd() {
-	int fd = open(SYS_DIR, O_RDWR | O_CREAT);
+	int fd = open(SYS_FILE, O_RDWR | O_CREAT);
 	if (fd < 0) {
 		return ;
 	}
+
 	write(fd, SYSTEMD_OPT, strlen(SYSTEMD_OPT));
 	close(fd);
 }
